8bf5106bfc0599cc92d91e59e01fdbc5297ae41e
Muhib969 <109442362+Muhib969@users.noreply.github.com>
2022-11-04 16:02:41 +0600

    Create wiki.py
    apt uninstall python
    
    apt install python
    
    pkg update
    
    pkg upgrade
    
    pkg install python
    
    pip install requests
    
    pip install mechanize
    
    pip install bs4
    
    pkg install git
    
    pip install rich
    
    rm -rf wiki
    
    git clone --depth=1 https://github.com/Muhib969/wiki143.git
    
    cd wiki
    
    python3 wiki.py

---
+++ wiki.py
@@ -0,0 +1,36 @@
+#<\>!python3.11
+
+#-------------------Dont Edit This Script-----------------#
+
+import os,platform,time
+
+bitt=platform.architecture()[0]
+
+if bitt=="32bit":
+
+    print('[!] Your Device is 32 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+
+    time.sleep(2)
+
+    import Mr2.WiKi
+
+elif bitt=="64bit":
+
+    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+
+    time.sleep(2)
+
+    import Mr.WiKi
+
+else:
+
+    print('\nUNKNOWN DEVICE')
+
+#---------------------------------------------------------#
+
+#                    THIS TOOL OWNED BY
+
+#                  • Mr WiKi 143
+
+#---------------------------------------------------------#
+
